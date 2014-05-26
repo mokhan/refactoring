952b787697393821e5d1cfc4295bee9f692e48a7
mo khan <mo@mokhan.ca>
2014-05-26 17:48:25 -0600

    replace conditional with polymorphism.

--- lib/movie.rb
+++ lib/movie.rb
@@ -10,18 +10,53 @@ class Movie
     @title, @price_code = title, price_code
   end
 
-  def amount_for(days_rented)
-    result = 0
+  def charge(days_rented)
+    price.charge(days_rented)
+  end
+
+  def frequent_renter_points(days_rented)
+    frequent_renter_points = 1
+    # add bonus for a two day new release rental
+    if price_code == Movie::NEW_RELEASE && days_rented > 1
+      frequent_renter_points += 1
+    end
+    frequent_renter_points
+  end
+
+  private
+
+  def price
     case price_code
     when Movie::REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
+      RegularPrice.new
     when Movie::NEW_RELEASE
-      result += days_rented * 3
+      NewReleasePrice.new
     when Movie::CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
+      ChildrensPrice.new
     end
+  end
+end
+
+class RegularPrice
+  def charge(days_rented)
+    result = 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+    result
+  end
+end
+
+class NewReleasePrice
+  def charge(days_rented)
+    result = 0
+    result += days_rented * 3
+    result
+  end
+end
+
+class ChildrensPrice
+  def charge(days_rented)
+    result = 1.5
+    result += (days_rented - 3) * 1.5 if days_rented > 3
     result
   end
 end
--- lib/rental.rb
+++ lib/rental.rb
@@ -6,15 +6,10 @@ class Rental
   end
 
   def amount
-    movie.amount_for(days_rented)
+    movie.charge(days_rented)
   end
 
   def points
-    frequent_renter_points = 1
-    # add bonus for a two day new release rental
-    if movie.price_code == Movie::NEW_RELEASE && days_rented > 1
-      frequent_renter_points += 1
-    end
-    frequent_renter_points
+    movie.frequent_renter_points(days_rented)
   end
 end
