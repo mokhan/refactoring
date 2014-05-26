297cfc33b53da9e4044681f0617b388d5cfed58b
mo khan <mo@mokhan.ca>
2014-05-26 17:21:20 -0600

    rename variable.

--- lib/customer.rb
+++ lib/customer.rb
@@ -44,10 +44,10 @@ class Customer
     result
   end
 
-  def points_for(element)
+  def points_for(rental)
     frequent_renter_points = 1
     # add bonus for a two day new release rental
-    if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+    if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
       frequent_renter_points += 1
     end
     frequent_renter_points
