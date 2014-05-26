30c03f5266684fe6d10db15cae5a9d10fb4c7ed9
mo khan <mo@mokhan.ca>
2014-05-26 17:23:27 -0600

    extract method to compute total points for all rentals.

--- lib/customer.rb
+++ lib/customer.rb
@@ -11,23 +11,29 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
+    total_amount = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
       this_amount = amount_for(element)
-      # add frequent renter points
-      frequent_renter_points += points_for(element)
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
       total_amount += this_amount
     end
     # add footer lines
     result += "Amount owed is #{total_amount}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "You earned #{total_points_for(@rentals)} frequent renter points"
     result
   end
 
   private
+
+  def total_points_for(rentals)
+    frequent_renter_points = 0
+    @rentals.each do |element|
+      frequent_renter_points += points_for(element)
+    end
+    frequent_renter_points
+  end
 
   def amount_for(rental)
     result = 0
